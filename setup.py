4ca6dcb9feeb289c415739962ab28cc79e0d26a7
0zean <42080997+0zean@users.noreply.github.com>
2025-10-16 03:44:08 -0400

    Populate setup file

---
+++ setup.py
@@ -0,0 +1,77 @@
+import sys
+
+import numpy as np
+import pybind11
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import Extension, find_packages, setup
+
+__version__ = "0.1.0"
+
+ext_modules = [
+    Pybind11Extension(
+        "fastgoertzel._fastgoertzel_core",
+        ["src/python_bindings.cpp", "src/goertzel.cpp"],
+        include_dirs=[
+            "src",
+            np.get_include(),
+        ],
+        cxx_std=17,  # Use C++17 standard
+        # Optimization flags
+        extra_compile_args=[
+            "-O3",                # Maximum optimization
+            "-march=native",      # Use CPU-specific instructions
+            "-ffast-math",        # Fast floating-point (be careful!)
+            "-Wall",              # All warnings
+            "-Wextra",            # Extra warnings
+            "-ffast-math",        # Fast floating-point
+            "-funroll-loops",     # Unroll loops
+            "-ftree-vectorize",   # Auto-vectorization
+            "-fopenmp",           # OpenMP parallelization 
+        ]
+        if sys.platform != "win32"
+        else ["/O2"],
+        define_macros=[("VERSION_INFO", __version__)],
+    ),
+]
+
+setup(
+    name="fastgoertzel",
+    version=__version__,
+    author="Nicholas Picini",
+    author_email="pd1138@protonmail.com",
+    url="https://github.com/0zean/fastgoertzel_cpp",
+    description="High-performance Goertzel algorithm implementation",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="python"),
+    package_dir={"": "python"},
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy>=2.2.6",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-benchmark",
+            "black",
+            "mypy",
+            "sphinx",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: C++",
+        "Topic :: Scientific/Engineering",
+    ],
+)
